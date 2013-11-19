232aebb58be4cab19ba7ea8eefaceafdcb382c9b
Robert Fischer <robert@3dslice.com>
2013-11-18 21:11:34 -0800

    Send email notifications functionality

--- website/dynamic/responder.py
+++ website/dynamic/responder.py
@@ -1,16 +1,28 @@
 import cgi
 import webapp2
 
+from google.appengine.api import mail
+
 class Emailer(webapp2.RequestHandler):
   def post(self):
-    self.response.write('<html><body>')
-    self.response.write('Name: ' + cgi.escape(self.request.get('name')))
-    self.response.write('Email: ' + cgi.escape(self.request.get('email')))
-    self.response.write('Phone: ' + cgi.escape(self.request.get('phone')))
-    self.response.write('Location: ' + cgi.escape(self.request.get('location')))
-    self.response.write('Event Details: ' + cgi.escape(self.request.get('details')))
-    self.response.write('</body></html>')
-
+	 form_name = cgi.escape(self.request.get('name'))
+     form_email = cgi.escape(self.request.get('email'))
+     form_phone = cgi.escape(self.request.get('phone'))
+     form_location = cgi.escape(self.request.get('location'))
+     form_details = cgi.escape(self.request.get('details'))
+     
+     sender_address = 'info@randcphotos.com'
+     user_address = 'info@randcphotos.com'
+     subject = 'New Photography Inquiry from %s' % form_name
+     body = """
+     Name: %s
+     Email: %s
+     Phone: %s
+     Location: %s
+     Event Details %s
+     """ % (form_name, form_email, form_phone, form_location, form_details)
+     mail.send_mail(sender_address, user_address, subject, body)
+     self.redirect('contact.html?s=1')
 
 application = webapp2.WSGIApplication([
   ('/form-response', Emailer),
